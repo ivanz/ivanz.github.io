394f3b39e9e5b8e19df615fc0098085da309522c
Ivan Zlatev <ivan@ivanz.com>
2022-11-24 22:30:34 +0000

    Another attempt

--- _includes/analytics.js
+++ _includes/analytics.js
@@ -1,5 +1,6 @@
 const script = document.createElement('script');
 script.src = 'https://www.googletagmanager.com/gtag/js?id=G-SX79B3V0WL';
+script.async = true;
 document.head.append(script);
 
 window.dataLayer = window.dataLayer || [];
