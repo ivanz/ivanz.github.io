247898d5d9a0788fd3092a7c787fa877d48a1e31
Ivan Zlatev <ivan@ivanz.com>
2022-11-24 22:22:18 +0000

    Attempt at re-enabling analytics

---
+++ _includes/analytics.js
@@ -0,0 +1,9 @@
+const script = document.createElement('script');
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-SX79B3V0WL';
+document.head.append(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-SX79B3V0WL');
